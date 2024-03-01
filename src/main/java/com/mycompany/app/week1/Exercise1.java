893d5ecb3f7f86ff388128b9329ff2ec976480d2
QuynhSuny <160729729+QuynhSuny@users.noreply.github.com>
2024-03-01 01:42:26 +0000

    Baitap If....Else

--- src/main/java/com/mycompany/app/week1/Exercise1.java
+++ src/main/java/com/mycompany/app/week1/Exercise1.java
@@ -2,7 +2,7 @@ package com.mycompany.app.week1;
 
 public class Exercise1 {
     public void testIf() {
-        if (19 > 18) {
+        if (20 > 18) {
         System.out.println("20 is greater than 18");
       }
     }  
