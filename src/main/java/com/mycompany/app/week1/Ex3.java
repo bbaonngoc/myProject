97d614ba6b7a171b8d058c2aeb303a7dfd1cdc2f
tuandeptraii <160729233+ahtuanzn@users.noreply.github.com>
2024-03-06 09:03:01 +0000

    tuananh

--- src/main/java/com/mycompany/app/week1/Ex3.java
+++ src/main/java/com/mycompany/app/week1/Ex3.java
@@ -2,7 +2,7 @@ package com.mycompany.app.week1;
 
 public class Ex3 {
   public void exc() {
-    String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+    String[] cars = {"toyota", "BMW", "Ford", "Mazda"};
     for (String i : cars) {
       System.out.println(i);
     }    
