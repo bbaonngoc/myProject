681dd24e9bf25f8021a81253a1ba8a87ff20935e
bbaonngoc <150327162+bbaonngoc@users.noreply.github.com>
2024-03-07 10:22:03 +0000

    modify

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -4,17 +4,14 @@
  *---------------------------------------------------------------------------------------*/
 package com.mycompany.app;
 
-import java.util.Scanner;
-import java.util.*;
-import java.io.*;
 import com.mycompany.app.week1.Exercise1;
 import com.mycompany.app.week1.Ex2;
 import com.mycompany.app.week1.Ex3;
 import com.mycompany.app.week1.Exercise4;
 import com.mycompany.app.week1.exTestSwitch;
 import com.mycompany.app.week1.exBreakContinue;
-import com.mycompany.app.week2.Methods;
-import com.mycompany.app.week2.Classes;
+import com.mycompany.app.week2.Code3.Methods;
+import com.mycompany.app.week2.Code3.Classes;
 
 public class App {
     public static void main(String[] args) {
